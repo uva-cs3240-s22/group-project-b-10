a4a540940947e6713c637963c94ce4a6e1a59699
Gaurav-R15 <gtr5ew@virginia.edu>
2022-03-01 13:22:25 -0500

    seventh commit

--- studyapp/urls.py
+++ studyapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'studyapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:meeting_id>/', views.detail, name='detail'),
--- studyapp/views.py
+++ studyapp/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Meeting
 
 # Create your views here.
 
 def index(request):
-    latest_reply_list = Meeting.objects.order_by('-post_date')[:5]
-    output = ', '.join([m.post_text for m in latest_reply_list])
-    return HttpResponse(output)
+    latest_post_list = Meeting.objects.order_by('-post_date')[:5]
+    template = loader.get_template('studyapp/index.html')
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+    return render(request, 'studyapp/index.html', context)
 
 def detail(request, meeting_id):
-    return HttpResponse("You're looking at meeting proposals %s." % meeting_id)
+    post = get_object_or_404(Meeting, pk = meeting_id)
+    return render(request, 'studyapp/detail.html', {'post': post})
 
 def results(request, meeting_id):
     response = "You're looking at the responses %s."
