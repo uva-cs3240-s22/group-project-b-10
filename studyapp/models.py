c398e300c38b047c943088028b2b93fe078d61b3
Gaurav-R15 <gtr5ew@virginia.edu>
2022-03-25 23:45:54 -0400

    User import in models.py

--- studyapp/models.py
+++ studyapp/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Create your models here.
 
