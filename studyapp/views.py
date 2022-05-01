64ffb31077302971dea0ae327b60e29ddc485a6f
uuilliammeyer <wmm6bm@virginia.edu>
2022-05-01 12:03:12 -0400

    fixed bug where the meeting wasn't appending to user profile upon submission of the form

--- studyapp/views.py
+++ studyapp/views.py
@@ -117,15 +117,16 @@ def CreateMeeting(request):
     template_name = 'studyapp/create-meetings.html'
     form = MeetingCreateForm(request.POST or None)
     if form.is_valid():
-        f = form.save()
-        print(f.id)
+        new_meeting = form.save()
+        # print(f.id)
         # append the meeting to user profile
-        # myProfile = Profile.objects.get(user = request.user)
+        myProfile = Profile.objects.get(user = request.user)
         # meeting_id = request.POST['meeting_id']
-        # meeting = Meeting.objects.get(id = meeting_id)
+        meeting_id = new_meeting.id
+        meeting = Meeting.objects.get(id = meeting_id)
 
-        # myProfile.meetings.add(meeting)
-        # myProfile.save()
+        myProfile.meetings.add(meeting)
+        myProfile.save()
         
     context = {
         'form': form 
