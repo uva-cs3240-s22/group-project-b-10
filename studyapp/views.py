23ee40c7614a5d7d0a7332c7d13d47b27033f68d
{oscardaum} <{odaum14@gmail.com}>
2022-05-02 23:48:31 -0400

    updated name for chat function

--- studyapp/views.py
+++ studyapp/views.py
@@ -250,7 +250,9 @@ def room_detail(request, slug):
 fake = Faker()
 
 def token(request):
-    identity = request.GET.get('identity', Profile.objects.get(user = request.user).name)
+    # identity = request.GET.get('identity', Profile.objects.get(user = request.user).name)
+    # print("name", Profile.objects.get(user = request.user).name, "type", type(er).Profile.objects.get(user = request.usname))
+    identity = request.GET.get('identity', fake.user_name())
     device_id = request.GET.get('device', 'default')  # unique device ID
 
     # print("token views")
@@ -272,6 +274,7 @@ def token(request):
 
     response = {
         'identity': identity,
+        'username': Profile.objects.get(user = request.user).name,
         'token': token.to_jwt()
     }
 
